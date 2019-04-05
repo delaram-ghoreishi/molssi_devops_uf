10dbf5b1ca105793afe7a4b812d14ad3a0f956bf
Delaram Ghoreishi <delaramq@gmail.com>
2019-04-05 13:34:36 -0400

    add mean function to molssi_math

--- molssi_devops_uf/molssi_math.py
+++ molssi_devops_uf/molssi_math.py
@@ -5,6 +5,24 @@ A sample repository for MolSSI Workshop at UF
 Handles the primary functions
 """
 
+def mean(num_list):
+    """
+    Calculate the mean/average of a list of numbers.
+
+    Parameters
+    ----------
+    num_list : list
+        The list to take the average of
+
+    Returns
+    -------
+    mean_list : float
+        The mean of the list
+    """
+    
+    mean_list = sum(num_list)/float(len(num_list))
+    return mean_list
+
 
 def canvas(with_attribution=True):
     """
